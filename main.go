4d0e69a0959cbac24306ee1c51239890a639d834
rmaftei <mafteiromeo@gmail.com>
2017-04-25 17:37:25 +0300

    First working draft

---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"strings"
+	"time"
+	"strconv"
+)
+
+const RESOURCE_URL = "https://www.super-liga.ro/superliga/program-rezultate/"
+const SELECTOR = "[style^=\"padding:2px;border-bottom:1px solid #ccc;vertical-align:middle;\"]"
+const STAGE_SIZE = 3
+
+type Stage struct {
+	games []Game
+}
+
+type Game struct {
+	startTime time.Time
+	location string
+	team1 string
+	team2 string
+	pointsTeam1 int
+	pointsTeam2 int
+	liveTransmission string
+
+}
+
+func main() {
+
+	var stages = make([]Stage, 0)
+
+	var result = make([]string, 0)
+
+	doc, err := goquery.NewDocument(RESOURCE_URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find(SELECTOR).Each(func(i int, s *goquery.Selection) {
+		text := strings.Replace(s.Text(), "\n", "", -1)
+		text = strings.Replace(text, "\t", "", -1)
+
+		if len(text) > 0 {
+			result = append(result, text)
+
+		}
+
+	})
+
+
+	currentStage := make([]Game, 0)
+
+	for i := 0; i < len(result); i+=6 {
+		data := result[i: i + 6]
+
+		dateTime := strings.Split(data[0], "ora")
+
+		date := strings.Split(dateTime[0], ".")
+		clock := strings.Split(strings.TrimSpace(dateTime[1]), ":")
+
+		year, _:= strconv.Atoi(date[2])
+		month, _:= strconv.Atoi(date[1])
+		day, _:= strconv.Atoi(date[0])
+		hour, _:= strconv.Atoi(clock[0])
+		minute, _:= strconv.Atoi(clock[1])
+
+		dateObject := time.Date(year, time.Month(month), day, hour, minute, 0, 0, time.UTC)
+
+		teams := strings.Split(data[3], " - ")
+		score := strings.Split(data[4], "-")
+		scoreTeam1, _ := strconv.Atoi(score[0])
+		scoreTeam2, _ := strconv.Atoi(score[1])
+
+		currentStage = append(currentStage, Game{
+			dateObject,
+			data[2],
+			teams[0],
+			teams[1],
+			scoreTeam1,
+			scoreTeam2,
+			data[5]})
+
+
+		if len(currentStage) % STAGE_SIZE == 0 {
+			stages = append(stages, Stage{currentStage})
+		}
+
+	}
+
+	fmt.Println(stages)
+
+}
